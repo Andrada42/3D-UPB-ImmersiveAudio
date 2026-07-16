b2c422416d5575936b582a0add11b089f632260f
Andrada42 <andradaangheloiu@gmail.com>
2026-07-16 08:43:36 +0300

    Feat: Complete homework task 2 (session 1 - day/night ambience crossfade)
    - Add Day and Night AudioSources with loop settings
    - Implement smooth crossfading for day and night ambience

--- 3D-UPB-ImmersiveAudio/Assets/Workshop/Scaffolding/Nature/Scripts/Audio/Manager/UnityAudioManager.cs
+++ 3D-UPB-ImmersiveAudio/Assets/Workshop/Scaffolding/Nature/Scripts/Audio/Manager/UnityAudioManager.cs
@@ -5,20 +5,39 @@ namespace Workshop.Scaffolding.Nature.Scripts.Audio.Manager
 {
     public class UnityAudioManager : AudioManager
     {
+        [Header("Footsteps")]
         public AudioSource footstepSource;
         public AudioClip[] footstepDirtClips;
         public AudioClip[] footstepStoneClips;
         public AudioClip[] footstepWoodClips;
 
+        [Header("Ambience")]
+        public AudioSource daySource;
+        public AudioSource nightSource;
+        public AudioClip dayAmbienceClip;
+        public AudioClip nightAmbienceClip;
 
         private void OnEnable()
         {
             fpsController.OnFootstepDetected += HandleFootstepDetected;
+            dayNightCycleController.OnDayNightCycleValueChanged += HandleDayNightCycleValueChanged;
         }
 
         private void OnDisable()
         {
             fpsController.OnFootstepDetected -= HandleFootstepDetected;
+            dayNightCycleController.OnDayNightCycleValueChanged -= HandleDayNightCycleValueChanged;
+        }
+
+        private void Start()    // folosim Start() ci nu Awake() pt a fi siguri ca totul e pregatit si AudioSource-urile folosite exista (daca erau configurate intr-o metoda Awake() altundeva ar fi fost important)
+        {
+            nightSource.volume = 1f;
+            daySource.clip = dayAmbienceClip;
+            daySource.Play();                   // Play() permite loop (setat din editor sau prin cod)
+                                                // PlayOneShot() nu permite
+            nightSource.volume = 0f;
+            nightSource.clip = nightAmbienceClip;
+            nightSource.Play();
         }
 
         private void HandleFootstepDetected(AudioUtils.AudioSurfaceType type, float speed)
@@ -50,6 +69,14 @@ namespace Workshop.Scaffolding.Nature.Scripts.Audio.Manager
                 case AudioUtils.AudioSurfaceType.None:break;
                 default: throw new ArgumentOutOfRangeException(nameof(type), type, null);
             }
+        }
+
+        private void HandleDayNightCycleValueChanged(float sliderValue)
+        {
+            Debug.Log($"Day-night cycle value change detected at {sliderValue}");
+
+            daySource.volume = 1f - sliderValue;
+            nightSource.volume = sliderValue;
         }
     }
 }
